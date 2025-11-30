03702e2310953e3b0ce94aa795c095a2b6f5acea
ayabelkacem <abelkacem412@gmail.com>
2025-11-30 18:53:10 +0100

    fix(zone): unsubscribe from/from-of subscriptions in FromOfComponent

--- src/app/rxjs/from-of/from-of.component.ts
+++ src/app/rxjs/from-of/from-of.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-import { from, of } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { from, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-from-of',
   templateUrl: './from-of.component.html',
   styleUrls: ['./from-of.component.css'],
 })
-export class FromOfComponent {
+export class FromOfComponent implements OnDestroy {
   data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  from$ = from(this.data).subscribe((data) => {
-    console.log(`from: ${data}`);
-  });
-  of$ = of(this.data).subscribe((data) => {
-    console.log(`of: ${data}`);
-  });
+  private fromSub: Subscription | null = null;
+  private ofSub: Subscription | null = null;
+
+  constructor() {
+    // subscribe in constructor but keep references so we can unsubscribe on destroy
+    this.fromSub = from(this.data).subscribe((data) => {
+      console.log(`from: ${data}`);
+    });
+    this.ofSub = of(this.data).subscribe((data) => {
+      console.log(`of: ${data}`);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.fromSub?.unsubscribe();
+    this.ofSub?.unsubscribe();
+  }
 }
